e4002651471d3217f80af4158717e3f4160a02ba
Tsiory Rakotoarimalala <80772886+tsiory-rovantsoa@users.noreply.github.com>
2023-04-20 18:20:13 +0300

    add service

--- src/app/pokemon/list-pokemon/list-pokemon.component.ts
+++ src/app/pokemon/list-pokemon/list-pokemon.component.ts
@@ -1,17 +1,21 @@
-import { Component } from '@angular/core';
-import { POKEMONS } from '../mock-pokemon-list';
+import { Component, OnInit } from '@angular/core';
 import { Pokemon } from '../pokemon';
 import { Router } from '@angular/router';
+import { PokemonService } from '../pokemon.service';
 
 @Component({
   selector: 'app-list-pokemon',
   templateUrl: './list-pokemon.component.html',
 })
-export class ListPokemonComponent {
-  pokemon: Pokemon[] = POKEMONS;
+export class ListPokemonComponent implements OnInit{
+  pokemon: Pokemon[];
 
-  constructor(private router:Router){
+  constructor(private router:Router,private pokemonService: PokemonService){
 
+  }
+
+  ngOnInit(): void {
+      this.pokemon = this.pokemonService.getPokemonList();
   }
 
   goToPokemon(pokemon:Pokemon){
